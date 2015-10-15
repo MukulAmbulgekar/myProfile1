15552f7ab7a68aa57bbd7abf94535250b2e5eb77
Mukul Ambulgekar <mukulambulgekar@gmail.com>
2015-10-14 23:36:35 -0700

    Added Projects page to portfolio.

--- client/app/preview/preview.controller.js
+++ client/app/preview/preview.controller.js
@@ -1,7 +1,37 @@
 'use strict';
 
 angular.module('myprofileApp')
-	.controller('PreviewCtrl', function($scope, userInfo, $location) {
+	.controller('PreviewCtrl', function($scope, userInfo, $location,$modal) {
+		$scope.items = ['item1', 'item2', 'item3'];
+var items;
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'project1.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+
 		$scope.message = 'Hello';
 		$scope.menuItem = [{
 			'title': 'Home',
@@ -67,3 +97,19 @@ angular.module('myprofileApp')
 			return monthNames[date.getMonth()] + ' ' + date.getFullYear();
 		}
 	});
+
+angular.module('myprofileApp').controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
